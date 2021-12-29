af7b965ecb6aadac3da0c98837c8fd6a7e2619df
nad1a0S <32828052+nad1a0S@users.noreply.github.com>
2021-12-28 23:19:26 -0500

    API - add new pet logic and sample files

---
+++ petstore.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, jsonify
+import json
+import pandas as pd
+
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return 'Hello Cool Cat!'
+
+@app.route('/pet', methods = ['POST', 'PUT'])
+def pet_records():
+    if request.method == 'PUT':
+        # Update an existing pet
+        return 1
+    if request.method == 'POST':
+        #Add new record
+        #Get new record from the request
+        record = json.loads(request.data)
+        #read data from txt file
+        with open('/Users/nsuguitan/git/pet-store-api/data/pet.txt', 'r') as f:
+            data = f.read()
+        if not data:
+            #If there is no data in the file, this is the first record
+            records = [record]
+        else:
+            #Otherwise append the record to existing records
+            records = json.loads(data)
+            #Checking to make sure the pet isn't already there
+            #If it is, we will not write the new pet and let the user know
+            for item in records:
+                #print("Item:",item['id'])
+                #print("record:",record['id'])
+                if item['id'] == record['id']:
+                    return 'That pet is already in the system dummy!'
+            records.append(record)
+        with open('/Users/nsuguitan/git/pet-store-api/data/pet.txt', 'w') as f:
+            #update the file
+            f.write(json.dumps(records, indent=2))
+        return jsonify(record)
+
+if __name__ == "__main__":
+    app.run(debug=True)
